74d5e475640934ad32cede2ce27c1ce5db7a4501
Cruel <machin3@gmail.com>
2014-09-23 23:31:26 -0400

    Add blog skeleton and lunr.js searching

---
+++ _posts/2014-9-25-Test-Post.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: Test Post
+author: Thomas Edvalson
+---
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus debitis eveniet magnam nostrum quam ratione reprehenderit sunt temporibus voluptatum? Ad, aut cumque dolorem, eius ex itaque minus molestias mollitia perspiciatis placeat quidem quis quod ut veniam voluptate voluptates voluptatum? Consectetur cumque eaque impedit itaque laborum numquam obcaecati reiciendis, similique!
+
+![My helpful screenshot](/assets/test.jpg){: style="float:right"}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusamus debitis eveniet magnam nostrum quam ratione reprehenderit sunt temporibus voluptatum? Ad, aut cumque dolorem, eius ex itaque minus molestias mollitia perspiciatis placeat quidem quis quod ut veniam voluptate voluptates voluptatum? Consectetur cumque eaque impedit itaque laborum numquam obcaecati reiciendis, similique!
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).foundation();
 
-$(document).ready(function(){
+$(function(){
 	$('#carousel').slick({
 		dots: true,
 		infinite: true,
@@ -13,4 +13,17 @@ $(document).ready(function(){
 		//slide: 'div',
 		//cssEase: 'linear',
 	});
+
+    if ($('#search-results').length) {
+        // Disable form submission because lunrSearch autoloads it
+        $('.top-bar form').submit(function(e){
+            e.preventDefault();
+        });
+        $('#search-query').lunrSearch({
+            indexUrl: '/search.json',             // URL of the `search.json` index data for your site
+            results:  '#search-results',          // jQuery selector for the search results container
+            entries:  '.entries',                 // jQuery selector for the element to contain the results list, must be a child of the results element above.
+            template: '#search-results-template'  // jQuery selector for the Mustache.js template
+        });
+    }
 });
